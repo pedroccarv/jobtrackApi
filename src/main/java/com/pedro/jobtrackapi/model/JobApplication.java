68597dd2eefb2a2698a2d75c4e35a7aa602798f0
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-04-30 17:41:29 -0300

    feat: add job application entity

---
+++ src/main/java/com/pedro/jobtrackapi/model/JobApplication.java
@@ -0,0 +1,27 @@
+package com.pedro.jobtrackapi.model;
+
+import com.pedro.jobtrackapi.enums.ApplicationStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class JobApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "job_opening_id")
+    private JobOpening jobOpening;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status;
+
+    private String notes;
+
+    private LocalDate appliedAt;
+
+
+}
