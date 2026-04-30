2810992085e5b7864c95b3858db28ccf74594f6b
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-04-30 17:39:12 -0300

    chore: initialize spring boot project

---
+++ src/test/java/com/pedro/jobtrackapi/JobtrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.jobtrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
