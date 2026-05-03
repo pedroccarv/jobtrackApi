2791bd487e1f190b03848425722f30c718902c78
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-05-03 15:22:24 -0300

    feat: add technology service

--- src/main/java/com/pedro/jobtrackapi/service/TechnologyService.java
+++ src/main/java/com/pedro/jobtrackapi/service/TechnologyService.java
@@ -1,8 +1,15 @@
 package com.pedro.jobtrackapi.service;
 
+import com.pedro.jobtrackapi.dto.technology.CreateTechnologyRequest;
+import com.pedro.jobtrackapi.dto.technology.TechnologyResponse;
+import com.pedro.jobtrackapi.dto.technology.UpdateTechnologyRequest;
+import com.pedro.jobtrackapi.model.Technology;
 import com.pedro.jobtrackapi.repository.TechnologyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +17,47 @@ public class TechnologyService {
 
     private final TechnologyRepository technologyRepository;
 
+    public TechnologyResponse findTechnologyById(Long id) {
+        Technology technology = findEntityById(id);
+        return toResponse(technology);
+    }
+
+    public List<TechnologyResponse> findAllTechnologies() {
+        List<Technology> technology = technologyRepository.findAll();
+        return technology.stream().map(this::toResponse).toList();
+    }
+
+    public TechnologyResponse createTechnology(CreateTechnologyRequest createTechnologyRequest) {
+        return  toResponse(technologyRepository.save(toEntity(createTechnologyRequest)));
+    }
+
+    public void deleteTechnology(Long id){
+        Technology technology = findEntityById(id);
+        technologyRepository.delete(technology);
+    }
+
+    public TechnologyResponse updateTechnology(Long id, UpdateTechnologyRequest updateTechnologyRequest) {
+        Technology entityById = findEntityById(id);
+        entityById.setName(updateTechnologyRequest.name());
+        return toResponse(technologyRepository.save(entityById));
+    }
+
+    private Technology findEntityById(Long id) {
+        return  technologyRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Technology not found"));
+    }
+
+
+    private TechnologyResponse toResponse(Technology technology) {
+        return new TechnologyResponse(
+                technology.getId(),
+                technology.getName()
+        );
+    }
+
+    private Technology toEntity(CreateTechnologyRequest technologyRequest) {
+        Technology technology = new Technology();
+        technology.setName(technologyRequest.name());
+        return technology;
+    }
 }
