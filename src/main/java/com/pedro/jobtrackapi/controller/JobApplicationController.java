1afaf8a56ca8c72e281c71a54f2b8daae2268f85
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-05-04 17:01:45 -0300

    fix: add @Valid to request bodies in JobApplicationController

--- src/main/java/com/pedro/jobtrackapi/controller/JobApplicationController.java
+++ src/main/java/com/pedro/jobtrackapi/controller/JobApplicationController.java
@@ -5,6 +5,7 @@ import com.pedro.jobtrackapi.dto.jobapplication.JobApplicationResponse;
 import com.pedro.jobtrackapi.dto.jobapplication.UpdateJobApplicationRequest;
 import com.pedro.jobtrackapi.model.JobApplication;
 import com.pedro.jobtrackapi.service.JobApplicationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,13 +33,13 @@ public class JobApplicationController {
     }
 
     @PostMapping
-    public ResponseEntity<JobApplicationResponse> createJobApplication(@RequestBody CreateJobApplicationRequest createJobApplicationRequest) {
+    public ResponseEntity<JobApplicationResponse> createJobApplication(@Valid @RequestBody CreateJobApplicationRequest createJobApplicationRequest) {
         JobApplicationResponse jobApplication = jobApplicationService.createJobApplication(createJobApplicationRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(jobApplication);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<JobApplicationResponse> updateJobApplication(@PathVariable Long id, @RequestBody UpdateJobApplicationRequest updateJobApplicationRequest) {
+    public ResponseEntity<JobApplicationResponse> updateJobApplication(@PathVariable Long id, @Valid @RequestBody UpdateJobApplicationRequest updateJobApplicationRequest) {
         JobApplicationResponse jobApplicationResponse = jobApplicationService.updateJobApplication(id, updateJobApplicationRequest);
         return ResponseEntity.ok(jobApplicationResponse);
     }
