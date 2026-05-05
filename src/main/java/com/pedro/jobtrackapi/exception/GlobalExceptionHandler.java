2a9944741664fb1b1e0fff00e2743d485ffe7f11
pedroccarv <pedrocarvalho.phm@gmail.com>
2026-05-05 09:48:29 -0300

    refactor: add custom applications exceptions

--- src/main/java/com/pedro/jobtrackapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pedro/jobtrackapi/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pedro.jobtrackapi.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,54 +14,58 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
-    public ApiError handleResponseStatusException(ResponseStatusException exception, HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleResponseStatusException(ResponseStatusException exception, HttpServletRequest request) {
         int statusCode = exception.getStatusCode().value();
 
-        HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+        HttpStatus httpStatus = HttpStatus.resolve(statusCode);
 
-        return new ApiError(
+        ApiError apiError = new ApiError(
                 LocalDateTime.now(),
                 statusCode,
                 httpStatus != null ? httpStatus.getReasonPhrase() : "Error",
                 exception.getReason(),
                 request.getRequestURI()
         );
+        return ResponseEntity.status(exception.getStatusCode()).body(apiError);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiError handleValidationException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException exception, HttpServletRequest request) {
         String message = exception.getFieldErrors()
                 .stream()
-                .map(error -> error.getField() + ":" + error.getDefaultMessage())
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
                 .findFirst().orElse("Validation Error");
-        return new ApiError(
+        ApiError apiError = new ApiError(
                 LocalDateTime.now(),
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 message,
                 request.getRequestURI()
         );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ApiError handleResourceNotFoundException(ResourceNotFoundException exception, HttpServletRequest request) {
-        return new ApiError(
+    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException exception, HttpServletRequest request) {
+        ApiError apiError = new ApiError(
                 LocalDateTime.now(),
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND.getReasonPhrase(),
                 exception.getMessage(),
                 request.getRequestURI()
         );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
 
     @ExceptionHandler(BusinessException.class)
-    public ApiError handleBusinessException(BusinessException exception, HttpServletRequest request) {
-        return new ApiError(
+    public ResponseEntity<ApiError> handleBusinessException(BusinessException exception, HttpServletRequest request) {
+        ApiError apiError =  new ApiError(
                 LocalDateTime.now(),
                 HttpStatus.CONFLICT.value(),
                 HttpStatus.CONFLICT.getReasonPhrase(),
                 exception.getMessage(),
                 request.getRequestURI()
         );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
     }
 }
